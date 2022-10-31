d4a771a6077d40a41e44eaacf83b7a67b53a3ab8
Maria Khudolii <maria.khudolii@gmail.com>
2022-10-31 10:45:48 +0200

    files added

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var seen = 0;
+$(window).scroll(function() {
+    var section_height = $('.results__list').offset().top - window.innerHeight;
+    if (seen == 0 && $(window).scrollTop() > section_height) {
+        $('.result__number').each(function() {
+            var $this = $(this),
+            countTo = $this.attr('data-count');
+            $({countNum: $this.text()}).animate({countNum: countTo},
+            {duration: 3000, easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        seen = 1;
+    }
+});
+
+$('.testimonial__slider').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow:1,
+  slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    dotsClass: 'slick-dots'
+});
